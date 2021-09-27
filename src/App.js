84e95a58e8ab50dfe2c15700deb0ff38bbafa796
rslaporte <rafanex@gmail.com>
2021-09-27 01:09:26 -0300

    Starting the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Foda-se</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
