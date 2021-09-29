aa85e5048da0c86497425e7dcef2dc0e9ba6638d
rslaporte <rafanex@gmail.com>
2021-09-28 21:17:50 -0300

    Request test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [data, setData] = useState({})
+
+  const api = axios.create({
+    baseURL: "http://localhost:8000",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+        "Content-Type": "application/json"
+    }
+  })
+
+  const getData = async () => {
+    const books = await api.get('http://localhost:8000/milluki/books')
+    console.log(books.data)
+  }
+
+  getData()
+
   return (
     <div className="App">
       <header className="App-header">
